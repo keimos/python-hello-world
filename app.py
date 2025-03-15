d797ac0b32af60b0bc3f7adbe9449e3438173ef5
keimos <keithmosleyjr@gmail.com>
2025-03-14 23:58:34 -0400

    Added dynamic route

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return jsonify({"message": "Hello, Khyri!"})  # Returns JSON response
 
+@app.route("/greeting/<name>")
+def greeting(name):
+    return jsonify({"message": f"Hello, {name}!"}) # Dynamic greeting
+
 if __name__ == "__main__":
     # Run the Flask app in debug mode
     app.run(debug=True)
