f0be8e364e49244375474225c853d570a242c265
keimos <keithmosleyjr@gmail.com>
2025-03-14 23:35:38 -0400

    Refactor to return responses in JSON

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return "Hello, Khyri!"  # Returns Hello World!
+    return jsonify({"message": "Hello, Khyri!"})  # Returns JSON response
+
+if __name__ == "__main__":
+    # Run the Flask app in debug mode
+    app.run(debug=True)
